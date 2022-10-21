77e137db89b9035eff8c09cebad5aeac51a11093
AgusCDT <acastrod@ucm.es>
2022-10-21 20:19:23 +0200

    .

--- src/Game.js
+++ src/Game.js
@@ -1,36 +1,38 @@
+  
+export default class Game extends Phaser.Scene{
   /*import { Shop } from './scenes/Shop.js'
   import { Menu } from './scenes/Menu.js'
   import { Tierra } from './scenes/Tierra.js'*/
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: {
-        preload:preload, create:create}
+    const config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 200 }
+            }
+        },
+        scene: {
+            preload:preload, create:create}
+            
         
-    
-  };
+      };
 
-  var game = new Phaser.Game(config);
+      var game = new Phaser.Game(config);
 
-function preload ()
-{
-    this.load.image("pavana", "./assets/imagenes/gaviota.png");
+    function preload ()
+    {
+        this.load.image("pavana", "./assets/imagenes/gaviota.png");
 
-    
+        
+    }
+
+    function create ()
+    {
+      this.pavana = this.physics.add.image(400, 300, "pavana");
+      this.pavana.setScale(0.5); 
+      
+      this.pavana.setCollideWorldBounds(true);
+    }
 }
-
-function create ()
-{
-  this.pavana = this.physics.add.image(400, 300, "pavana");
-  this.pavana.setScale(0.5); 
-  this.pavana.flipY = true;
-  this.pavana.setCollideWorldBounds(true);
-}
-
