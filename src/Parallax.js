12700bc20d64ced96a77e282723b9fba288af00e
AgusCDT <acastrod@ucm.es>
2022-10-25 15:42:35 +0200

    Parallax (a priori)

---
+++ src/Parallax.js
@@ -0,0 +1,18 @@
+export default class Parallax{
+
+	constructor(scene){
+		this.relatedScene = scene;
+	}
+
+	preload(){
+		this.relatedScene.load.image('parallax', './assets/escenarios/Tierra/Carretera/Carretera.jpg');
+	}
+
+	create(){
+		this.parallax = this.relatedScene.add.tileSprite(0, 0, 3200, 600, 'parallax').setOrigin(0,0);		
+	}
+
+	update(){
+		this.parallax.tilePositionX += 0.75;
+	}
+}
