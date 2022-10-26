657788c60ccd0d8d3d83f1371a10f9f1db40734c
PabloCerrada <pcvatm@gmail.com>
2022-10-26 17:20:15 +0200

    Update Pavana.js

--- src/entities/Pavana.js
+++ src/entities/Pavana.js
@@ -12,22 +12,17 @@ export default class Pavana extends Phaser.GameObjects.Sprite{
 		this.deceleration = 10;
 		this.cursors = this.scene.input.keyboard.createCursorKeys();		
 	}
-	
-	preUpdate() {
-	    if (this.cursors.up.isDown) {
-	    	//this.body.setVelocityY(this.speedY);
+
+	inputMovement() {
+		if (this.cursors.up.isDown) {
 	    	if (this.speedY > -this.maxSpeed) {
 				this.speedY -= this.acceleration;
-				console.log(this.speedY);
 	    	}
-	    	console.log("Arriba2");
 	    }
 	    else if (this.cursors.down.isDown) {
-	    	//this.body.setVelocityY(this.speedY);
 	    	if (this.speedY < this.maxSpeed) {
 				this.speedY += this.acceleration;
 	    	}
-	    	console.log("Abajo2");
 	    }
 	    else {
 		   	if (this.speedY > 0){
@@ -35,30 +30,23 @@ export default class Pavana extends Phaser.GameObjects.Sprite{
 	    		if (this.speedY < 0){
 	    			this.speedY = 0;
 	    		}
-	    		console.log("Recuperando abajo");
 	    	}
 	    	if (this.speedY < 0){
 	    		this.speedY += this.deceleration;
 	    		if (this.speedY > 0){
 	    			this.speedY = 0;
 	    		}
-	    		console.log("Recuperando arriba");
 	    	}
-	    	//this.body.setVelocityY(this.speedY);
 	    }
 	    if (this.cursors.left.isDown) {
-	    	//this.body.setVelocityX(this.speedX);
 	    	if (this.speedX > -this.maxSpeed) {
 				this.speedX -= this.acceleration;
 	    	}
-	    	console.log("Izquierda");
 	    }
 	    else if (this.cursors.right.isDown) {
-	    	//this.body.setVelocityX(this.speedX);
 	    	if (this.speedX < this.maxSpeed) {
 				this.speedX += this.acceleration;
 	    	}
-	    	console.log("Derecha");
 	    }
 	    else {
 	    	if (this.speedX > 0){
@@ -66,18 +54,18 @@ export default class Pavana extends Phaser.GameObjects.Sprite{
 	    		if (this.speedX < 0) {
 	    			this.speedX = 0;
 	    		}
-	    		console.log("Recuperando derecha");
 	    	}
 	    	else if (this.speedX < 0){
     			this.speedX += this.deceleration;
 	    		if (this.speedX > 0) {
 	    			this.speedX = 0;
 	    		}
-	    		console.log("Recuperando izquierda");
 	    	}
-	    	//this.body.setVelocityX(this.speedX);
 	    }
+	}
+	
+	preUpdate() {
+		inputMovement();
 	    this.body.setVelocity(this.speedX, this.speedY);
-	    console.log("Es la buena 2");
 	}
 }
