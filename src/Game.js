5dfe0b9d1c461a961de9087a420f5a4c5d41763a
AgusCDT <acastrod@ucm.es>
2022-10-28 14:20:40 +0200

    web

--- src/Game.js
+++ src/Game.js
@@ -3,12 +3,23 @@ import Menu from './scenes/Menu.js';
 import Tierra from './scenes/Tierra.js';
 
     let config = {
-        type: Phaser.AUTO,
+        type: Phaser.CANVAS,
+        canvas: document.getElementById("juego"),
         width: 800,
         height: 600,
         pixelArt: true,
         scale: {
-            autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+            autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+            mode: Phaser.Scale.FIT,
+            min: {
+            width: 256,
+            height: 192
+            },
+            max: {
+                width: 800,
+                height: 600 
+            },
+            zoom: 1
         },
         scene: [Menu,Tierra, Shop],           
         physics: { 
