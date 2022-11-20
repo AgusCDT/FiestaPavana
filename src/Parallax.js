402afd74df25192e17d078975f0f001edc82f76e
Cao33 <pablocao@ucm.es>
2022-11-20 02:19:20 +0100

    Imagenes y separacion de escenarios
    Ya conseguido la separacion de escenarios, he añadido varias imagenes y  que se eliminen los elementos en juego al cambiar de escenario

--- src/Parallax.js
+++ src/Parallax.js
@@ -1,15 +1,23 @@
 export default class Parallax extends Phaser.GameObjects.Sprite{
 
-	constructor(scene){
+	constructor(scene)
+	{
 		super(scene);
 		this.scene.add.existing(this);
-		this.parallax = this.scene.add.tileSprite(0, 0, 3200, 600, 'road').setOrigin(0,0);		
+		this.parallax = this.scene.add.tileSprite(0, 0, 3200, 600, 'road').setOrigin(0,0);	
+		this.id='road';	
 	}
-	changeBackground(id){
+	changeBackground(id)
+	{
 		this.parallax = this.scene.add.tileSprite(0, 0, 3200, 600, id).setOrigin(0,0);	
-		console.log(id);
+		this.id=id;
 	}
 	update(){
 		this.parallax.tilePositionX += 0.75;
+	}
+
+	checkId()
+	{
+		return this.id;
 	}
 }
