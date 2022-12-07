f712bcae90b8311b7aec8056e65bf48247c9a276
Beitarr <92715709+Beitarr@users.noreply.github.com>
2022-12-07 19:21:03 +0100

    Sonido de compra

--- src/components/ShopButton.js
+++ src/components/ShopButton.js
@@ -1,9 +1,8 @@
 import Button from './Button.js';
 
 export default class ShopButton extends Button {
-    constructor(scene, cloud, audio) {
-        super(scene, 'shopbutton', 600, 450, audio);
-        this.purchase = audio;
+    constructor(scene, cloud) {
+        super(scene, 'shopbutton', 600, 450);
         this.cloud = cloud;
     }
 
